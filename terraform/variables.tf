30f3d52be33cacf2f2a34e40348c45a656b2c2b5
loboff1 <60606168+loboff1@users.noreply.github.com>
2022-03-20 11:15:33 +0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -46,8 +46,14 @@ variable "network_tier" {
   default     = "PREMIUM"
 }
 
-variable "ssh_keys" {
-  description = "ssh-keys data"
+variable "ssh_user" {
+  description = "ssh username"
   type        = string
-  default     = "lobofffailodrom:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDYbR2t+mAILRS1ZjkT+T7WUgzhRFXh/AjwyxpthexVYmmjN1YB4otY08kJ1/i+f5Vo3jPLlIYRZMWcMnJ6e7slKUBEumrzsDhUWhwnZG4+F/eGyzm+18NIgjBbT40kflHL6/EbyxDm4cxU3DLKtjxJZHbj9iHS3PVWbJ/AKp11+sfIExdd7btcnHqtR3f1XZyyhxkiDas9qAwnsNnVp+nJp8bQpXifc4shEB4f2chcuPd+I6IK4+oEr8KKLcgB7fwrtJnINuESNuxjVdIYwXUPCiRGWnmF8bz63nOTDHgIKwUShL+2dWwz63MCJ4ZahddI2JnM/ocY5QTrltQ42GQdx1Y4leyXanzVw1wyXnK5q2y4MKl2d9JBRrS87K6yVL2+Juu8CRqBN8H64zmPKjwmEjvtqISb6fzWQUVVBLH29oI4TDKRkGkSTPptMB5WEJ8jWx0aWY3A9mdCg42YMyK/TLEcyXWj0ShgpEWOp9VvRyaMEx+EWvWAH3OWrXrZTPU= lobofffailodrom"
+  default     = "lobofffailodrom"
+}
+
+variable "ssh_pub_key_file" {
+  description = "ssh_pub_key_file name"
+  type        = string
+  default     = "key.pub"
 }
