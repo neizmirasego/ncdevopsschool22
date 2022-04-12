a58708286535eed2d9b658fdb8b0e18291330bb5
loboff1 <60606168+loboff1@users.noreply.github.com>
2022-04-12 22:44:38 +0300

    Update terraform.tf

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,29 +1,16 @@
-#1 VIRTUAL MACHINE
-resource "google_compute_instance" "vm_dev1" {
-  name         = "dev1-gh"
-  machine_type = var.machine_type
-  zone         = var.zone
-  boot_disk {
-    initialize_params {
-      size  = "20"
-      image = "ubuntu-os-cloud/ubuntu-2004-lts"
-    }
-  }
-  network_interface {
-    network = var.network
-    access_config {
-    }
-  }
-  metadata = {
-    ssh-keys = "${var.ssh_user}:${file(var.ssh_pub_key_file)}"
-  }
+# CI & DEV VIRTUAL MACHINES
+
+resource "google_compute_address" "static" {
+  for_each  = var.vm_name
+  name      = "${each.key}-ipv4"
+  region    =  var.region
 }
-#2 VIRTUAL MACHINE
-resource "google_compute_instance" "vm_ci_l2" {
-  name         = "ci-l2-gh"
-  machine_type = var.machine_type
-  zone         = var.zone
-  tags         = var.main_tags
+resource "google_compute_instance" "ncdev" {
+  for_each      = var.project_name
+  name          = "${each.key}-gh"
+  machine_type  = var.machine_type
+  zone          = var.zone
+  tags          = var.main_tags
   boot_disk {
     initialize_params {
       size  = "30"
@@ -33,13 +20,15 @@ resource "google_compute_instance" "vm_ci_l2" {
   network_interface {
     network = var.network
     access_config {
+            nat_ip = google_compute_address.static[each.key].address
     }
   }
   metadata = {
     ssh-keys = "${var.ssh_user}:${file(var.ssh_pub_key_file)}"
   }
 }
-#3 VIRTUAL MACHINE
+
+#REGISTRY VIRTUAL MACHINE
 resource "google_compute_instance" "registry" {
   name         = "registry"
   machine_type = var.machine_type
@@ -62,13 +51,4 @@ resource "google_compute_instance" "registry" {
   metadata = {
     ssh-keys = "${var.ssh_user}:${file(var.ssh_pub_key_file)}"
   }
-}
-output "registry_ip" {
-  value = google_compute_instance.registry.network_interface.0.access_config.0.nat_ip
-}
-output "vm_dev1_ip" {
-  value = google_compute_instance.vm_dev1.network_interface.0.access_config.0.nat_ip
-}
-output "vm_ci_l2_ip" {
-  value = google_compute_instance.vm_ci_l2.network_interface.0.access_config.0.nat_ip
 }
