b0be7bdf746d55d8ceed49880d51589c45cc1045
loboff1 <60606168+loboff1@users.noreply.github.com>
2022-04-12 23:22:30 +0300

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -2,9 +2,7 @@ output "registry_ip" {
   value = google_compute_instance.registry.network_interface.0.access_config.0.nat_ip
 }
 output "vm_ip" {
- value = [
-    for ncdev in google_compute_instance.ncdev : google_compute_instance.ncdev.network_interface.0.access_config.0.nat_ip
-  ]
+ value = google_compute_instance.ncdev[*].network_interface.0.access_config.0.nat_ip
 }
 #output "vm_ci_l2_ip" {
 #  value = google_compute_instance.vm_ci_l2.network_interface.0.access_config.0.nat_ip
